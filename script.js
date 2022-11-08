855b198cbac0bf79acde45b4e2c6d33e6a64065f
isaak.banquiles@hotmail.com <104627649+risaaak@users.noreply.github.com>
2022-11-08 13:25:35 +0800

    Add event to change tile color

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const container = document.querySelector('.container')
-for(let i=0;i<16;i++){ 
+for(let i=0;i<16;i++){ //column
+    for(let j=0;j<16;j++) { //row
         const tile = document.createElement('div');
         tile.classList = 'tile';
         container.appendChild(tile);
-};
+    }
+}
+
+selectTile = document.getElementsByClassName('tile');
+
+//change tile color on mouse hover
+for(let i=0;i<selectTile.length;i++){
+    selectTile[i].addEventListener("mouseover" ,function(e) {
+        e.target.style.backgroundColor = 'salmon'
+    })
+}
+
+
+
+
